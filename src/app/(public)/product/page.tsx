890a43c69abf260e3276b77ea6af4e0f5a2d9ea6
sultanIlyasa <sultan21004@mail.unpad.ac.id>
2024-06-19 06:28:51 +0700

    feat-products-view

--- src/app/(public)/product/page.tsx
+++ src/app/(public)/product/page.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { Metadata } from 'next'
 import { Product } from "@prisma/client";
 import { useState } from 'react';
+import { getAllProducts } from '@/lib/actions/product';
+import ProductsView from '@/components/products-page/products-view';
 
-const ProductPage = () => {
-  return (
-    <div>
-      <h1>Product Page</h1>
-    </div>
+export default async function ProductsPage() {
+  const products = await getAllProducts();
+  return(
+    <ProductsView products={products} />
   )
 }
 
-export default ProductPage
