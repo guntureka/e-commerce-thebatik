d022f28be5a70b739f12ec4178baa013ca5fbd20
daffayusranizar <daffa28.dn@gmail.com>
2024-06-19 04:02:39 +0700

    api-test

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -3,110 +3,17 @@ import { getAllUser } from "@/lib/actions/user";
 import { categorySchema, productSchema } from "@/lib/schemas";
 import { db } from "@/utils/db";
 import { NextRequest } from "next/server";
+import { createWishlist, deleteWishlist, updateWishlist } from "@/lib/actions/wishlist";
+import { createCategory } from "@/lib/actions/category";
+import { deleteCategory } from "@/lib/actions/category";
+import { uploadFile } from "@/lib/actions/file";
+import { createProduct } from "@/lib/actions/product";
+import { createCart, deleteCart } from "@/lib/actions/cart";
 
-async function parseFormData(formData: FormData) {
-  const entries = await formData.entries();
-  const data = Object.fromEntries(entries);
-  return data;
+const test  = async () => {
+  const result = await deleteCart(
+    'clxkvjxgd00082tir1f61tno9'
+  )
 }
 
-export const GET = async () => {
-  const data = await getAllCategories();
-
-  console.log("data;", data);
-  return Response.json(data);
-};
-
-export const POST = async (req: NextRequest) => {
-  const formData = await req.formData();
-  const entries = formData.entries();
-  const parsedData = Object.fromEntries(entries);
-
-  return Response.json(parsedData)
-
-  const validatedFields = productSchema.safeParse(parsedData);
-
-  if (!validatedFields.success) {
-    return Response.json({
-      error: "invalid fields",
-    });
-  }
-
-  const res = validatedFields.data;
-
-  return Response.json({
-    res,
-  });
-
-  // try {
-  //   await db.category.create({
-  //     data: {
-  //       name: res.name,
-  //       description: res.description,
-  //     },
-  //   });
-
-  //   return Response.json({
-  //     success: "Category created successfully",
-  //   });
-  // } catch (error) {
-  //   return Response.json({
-  //     error: "Something went wrong",
-  //   });
-  // }
-};
-
-export const PUT = async (req: NextRequest) => {
-  const params = req.nextUrl.searchParams;
-  const id = params.get("id");
-  const data = await req.json();
-  const validatedFields = categorySchema.safeParse(data);
-
-  if (!validatedFields.success) {
-    return Response.json({
-      error: "invalid fields",
-    });
-  }
-
-  const res = validatedFields.data;
-
-  try {
-    await db.category.update({
-      data: {
-        name: res.name,
-        description: res.description,
-      },
-      where: {
-        id: id!,
-      },
-    });
-
-    return Response.json({
-      success: "Category created successfully",
-    });
-  } catch (error) {
-    return Response.json({
-      error: "Something went wrong",
-    });
-  }
-};
-
-export const DELETE = async (req: NextRequest) => {
-  const params = req.nextUrl.searchParams;
-  const id = params.get("id");
-  try {
-    await db.category.delete({
-      where: {
-        id: id!,
-      },
-    });
-
-    return Response.json({
-      success: "Category created successfully",
-    });
-  } catch (error) {
-    return Response.json({
-      error: "Something went wrong",
-    });
-  }
-};
+test()
