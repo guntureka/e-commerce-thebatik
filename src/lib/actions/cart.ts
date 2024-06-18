48687213c74b72b8a75c9b06dd4f0e120ccd6813
guntureka <guntureka1020@gmail.com>
2024-06-19 02:03:43 +0700

    tete

--- src/lib/actions/cart.ts
+++ src/lib/actions/cart.ts
@@ -1,58 +1,91 @@
 import { db } from "@/utils/db";
-export const createCart = async (data: any) => {
-    try {
-      const cart = await db.cart.create({ data });
-      return cart;
-    } catch (error) {
-      return null;
-    }
-  };
-  
+import { z } from "zod";
+import { cartSchema } from "../schemas";
+export const createCart = async (data: z.infer<typeof cartSchema>) => {
+  try {
+    const validatedFields = cartSchema.safeParse(data);
 
-  export const getCartById = async (id: string) => {
-    try {
-      const cart = await db.cart.findUnique({
-        where: {
-          id,
-        },
-      });
-      return cart;
-    } catch (error) {
-      return null;
+    if (!validatedFields.success) {
+      return {
+        error: "invalid fields",
+      };
     }
-  };
-  
 
-  export const getAllCarts = async () => {
-    try {
-      const carts = await db.cart.findMany({});
-      return carts;
-    } catch (error) {
-      return null;
-    }
-  };
-  
+    const res = validatedFields.data;
 
-  export const updateCart = async (id: string, data: any) => {
-    try {
-      const cart = await db.cart.update({
-        where: { id },
-        data,
-      });
-      return cart;
-    } catch (error) {
-      return null;
-    }
-  };
-  
+    await db.cart.create({ data: res });
 
-  export const deleteCart = async (id: string) => {
-    try {
-      const cart = await db.cart.delete({
-        where: { id },
-      });
-      return cart;
-    } catch (error) {
-      return null;
+    return {
+      success: "Cart created successfully",
+    };
+  } catch (error) {
+    return {
+      error: "Something went wrong",
+    };
+  }
+};
+
+export const getCartById = async (id: string) => {
+  try {
+    const cart = await db.cart.findUnique({
+      where: {
+        id,
+      },
+    });
+    return cart;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const getAllCarts = async () => {
+  try {
+    const carts = await db.cart.findMany({});
+    return carts;
+  } catch (error) {
+    return null;
+  }
+};
+
+export const updateCart = async (
+  id: string,
+  data: z.infer<typeof cartSchema>
+) => {
+  try {
+    const validatedFields = cartSchema.safeParse(data);
+
+    if (!validatedFields.success) {
+      return {
+        error: "invalid fields",
+      };
     }
-  };
+
+    const res = validatedFields.data;
+
+    await db.cart.update({
+      where: {
+        id,
+      },
+      data: res,
+    });
+
+    return {
+      success: "Cart updated successfully",
+    };
+  } catch (error) {
+    return {
+      error: "Something went wrong",
+    };
+  }
+};
+
+export const deleteCart = async (id: string) => {
+  try {
+    const cart = await db.cart.delete({
+      where: { id },
+    });
+    return cart;
+  } catch (error) {
+    return null;
+  }
+};
